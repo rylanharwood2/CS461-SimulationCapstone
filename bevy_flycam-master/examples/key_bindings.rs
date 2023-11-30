5a0eee34544e41bc0fc3e2a822a2c7b22ea6d849
taye-r <102688112+taye-r@users.noreply.github.com>
2023-11-29 19:59:42 -0800

    Modified Flycam
    Changed pre-existing flycam controls to function like a plane

---
+++ bevy_flycam-master/examples/key_bindings.rs
@@ -0,0 +1,55 @@
+use bevy::prelude::*;
+use bevy_flycam::prelude::*;
+
+//From bevy examples:
+//https://github.com/bevyengine/bevy/blob/latest/examples/3d/3d_scene.rs
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PlayerPlugin)
+        .insert_resource(MovementSettings {
+            sensitivity: 0.00015, // default: 0.00012
+            speed: 12.0,          // default: 12.0
+        })
+        // Unreal movement layout
+        .insert_resource(KeyBindings {
+            move_ascend: KeyCode::E,
+            move_descend: KeyCode::Q,
+            ..Default::default()
+        })
+        .add_systems(Startup, setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {
+            size: 5.0,
+            ..Default::default()
+        })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    },));
+
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+
+    // light
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+}
